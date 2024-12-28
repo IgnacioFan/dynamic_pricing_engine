f5f12dec9aeda11f6361286e91ac9d86a8a80daf
Weilong <fan01856472@gmail.com>
2024-12-28 17:13:41 -0500

    design calculate_dynamic_price v2

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,6 @@ class Product
   field :competitor_price, type: BSON::Decimal128
   field :default_price, type: BSON::Decimal128
   field :dynamic_price, type: BSON::Decimal128
-  field :dynamic_price_expried_at, type: DateTime, default: -> { Time.now.utc + 3.hours }
 
   field :price_floor, type: BSON::Decimal128
 
@@ -27,7 +26,17 @@ class Product
   field :total_inventory, type: Integer, default: 0
   field :total_reserved, type: Integer, default: 0
 
-  # use current_demand_count and previous_demand_count to track if the product is high in demand
+  # use dynamic_price_expried_at to prevent price fluctuation
+  field :dynamic_price_expried_at, type: DateTime
+  # use dynamic_price_period to set the next price expiry
+  field :dynamic_price_period, type: Integer, default: 3
+  # use inventory_rates to calculate inventory_factor
+  field :inventory_rates, type: Hash, default: { very_high: -0.30, high: -0.15, medium: -0.05, low: 0, very_low: 0.10 }
+  # use demand_rates to calculate demand_factor
+  field :demand_rates, type: Hash, default: { high: 0.05, medium: 0.025, low: 0 }
+  # use inventory_thresholds to scope the product's inventory level
+  field :inventory_thresholds, type: Hash, default: { very_low: 95, low: 80, medium: 60, high: 40, very_high: 20 }
+  # use current_demand_count and previous_demand_count to scope product's demand level
   field :current_demand_count, type: Integer, default: 0
   field :previous_demand_count, type: Integer, default: 0
 
@@ -36,10 +45,12 @@ class Product
   validates :name, presence: true
   validates :name, uniqueness: { scope: :category }
   validates :category, presence: true
-  validates :inventory_level, inclusion: { in: [ :low, :medium, :high ] }
-  validates :demand_level, inclusion: { in: [ :low, :high ] }
+  validates :inventory_level, inclusion: { in: [ :very_low, :low, :medium, :high, :very_high ] }
+  validates :demand_level, inclusion: { in: [ :low, :medium, :high ] }
 
   before_save :set_price_floor, :set_default_dynamic_price
+  before_save :set_dynamic_price_expried_at
+
 
   def calculate_dynamic_price
     return dynamic_price if Time.now.utc <= dynamic_price_expried_at
@@ -72,27 +83,65 @@ class Product
     self.dynamic_price = [ demand_adjusted_price, competitor_price ].compact.min
   end
 
+  def calculate_dynamic_price_v2
+    return if Time.now.utc <= dynamic_price_expried_at
+
+    new_price = default_price + inventory_factor + demand_factor
+
+    self.dynamic_price_expried_at = Time.now.utc + dynamic_price_period.hours
+    self.dynamic_price = [ new_price, competitor_price ].compact.min
+  end
+
+  def inventory_factor
+    case inventory_level
+    when :very_high
+      default_price * inventory_rates[:very_high]
+    when :high
+      default_price * inventory_rates[:high]
+    when :medium
+      default_price * inventory_rates[:medium]
+    when :low
+      default_price * inventory_rates[:low]
+    when :very_low
+      default_price * inventory_rates[:very_low]
+    end
+  end
+
+  def demand_factor
+    case demand_level
+    when :high
+      default_price * demand_rates[:high]
+    when :medium
+      default_price * demand_rates[:medium]
+    when :low
+      default_price * demand_rates[:low]
+    end
+  end
+
   def set_inventory_level
-    if total_inventory.zero?
-      self.inventory_level = :high
+    inventory_ratio = total_reserved.to_f / total_inventory
+
+    self.inventory_level = case inventory_ratio
+    when inventory_thresholds[:very_low]...1
+      :very_low
+    when inventory_thresholds[:low]...inventory_thresholds[:very_low]
+      :low
+    when inventory_thresholds[:medium]...inventory_thresholds[:low]
+      :medium
+    when inventory_thresholds[:high]...inventory_thresholds[:medium]
+      :high
     else
-      available_ratio = (total_inventory - total_reserved).to_f / total_inventory
-      self.inventory_level = case available_ratio
-      when 0.0...INVENTORY_LOW_BAR
-        :low
-      when INVENTORY_LOW_BAR...INVENTORY_HIGH_BAR
-        :medium
-      else
-        :high
-      end
+      :very_high
     end
   end
 
   def set_demand_level
-    self.demand_level = if previous_demand_count.zero?
-      :low
+    self.demand_level = if current_demand_count > previous_demand_count
+      :high
+    elsif current_demand_count == previous_demand_count
+      :medium
     else
-      current_demand_count > previous_demand_count ? :high : :low
+      :low
     end
   end
 
@@ -125,5 +174,9 @@ class Product
 
   def set_default_dynamic_price
     self.dynamic_price ||= default_price
+  end
+
+  def set_dynamic_price_expried_at
+    self.dynamic_price_expried_at ||= Time.now.utc + dynamic_price_period.hours
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -72,41 +72,173 @@ RSpec.describe Product, type: :model do
     end
   end
 
+  describe 'callback' do
+    context "When dynamic_price_period is 2" do
+      let(:product) { create(:product, dynamic_price_period: 2) }
+
+      it "set the dynamic_price_expried_at field to 2 hours later" do
+        product_expiration = (Time.now.utc + 2.hours).strftime("%Y-%m-%d %H:%M:%S")
+        expect(product.dynamic_price_expried_at.strftime("%Y-%m-%d %H:%M:%S")).to eq(product_expiration)
+      end
+    end
+  end
+
+  describe '#dynamic_price_v2' do
+    let(:dynamic_price_period) { 3 }
+    let(:competitor_price) { nil }
+    let!(:product) do
+      create(
+        :product,
+        competitor_price:,
+        default_price: 100.0,
+        dynamic_price: 95.0,
+        inventory_level: :very_low,
+        demand_level: :high,
+        inventory_rates: { very_high: -0.30, high: -0.15, medium: -0.05, low: 0, very_low: 0.10 },
+        demand_rates: { high: 0.05, medium: 0.025, low: 0 },
+        dynamic_price_expried_at:,
+        dynamic_price_period:
+      )
+    end
+
+    context "When product's price has not expired" do
+      let(:dynamic_price_expried_at) { Time.now.utc + dynamic_price_period.hours }
+
+      it "returns the current product price" do
+        product.calculate_dynamic_price_v2
+        expect(product.dynamic_price).to eq(95.0)
+        expect(product.dynamic_price_expried_at).to eq(dynamic_price_expried_at)
+      end
+    end
+
+    context "When product's price expired" do
+      let(:dynamic_price_expried_at) { dynamic_price_period.hours.ago.utc }
+
+      context "when product is in very low inventory level and high demand level" do
+        it "returns the current product price" do
+          product.calculate_dynamic_price_v2
+          expect(product.dynamic_price).to eq(115.0)
+          expect(product.dynamic_price_expried_at > dynamic_price_expried_at).to eq(true)
+        end
+      end
+
+      context "when competitior price is lower than new product price" do
+        let(:competitor_price) { 108.0 }
+
+        it "returns the current product price" do
+          product.calculate_dynamic_price_v2
+          expect(product.dynamic_price).to eq(108.0)
+          expect(product.dynamic_price_expried_at > dynamic_price_expried_at).to eq(true)
+        end
+      end
+    end
+  end
+
+  describe "#inventory_factor" do
+    let(:inventory_rates) { { very_high: -0.30, high: -0.15, medium: -0.05, low: 0, very_low: 0.10 } }
+
+    context "when product's inventory level is very low" do
+      let!(:product) { build(:product, default_price: 100, inventory_level: :very_low, inventory_rates:) }
+
+      it { expect(product.inventory_factor).to eq(10) }
+    end
+
+    context "when product's inventory level is low" do
+      let!(:product) { build(:product, default_price: 100, inventory_level: :low, inventory_rates:) }
+
+      it { expect(product.inventory_factor).to eq(0) }
+    end
+
+    context "when product's inventory level is medium" do
+      let!(:product) { build(:product, default_price: 100, inventory_level: :medium, inventory_rates:) }
+
+      it { expect(product.inventory_factor).to eq(-5) }
+    end
+
+    context "when product's inventory level is high" do
+      let!(:product) { build(:product, default_price: 100, inventory_level: :high, inventory_rates:) }
+
+      it { expect(product.inventory_factor).to eq(-15) }
+    end
+
+    context "when product's inventory level is very high" do
+      let!(:product) { build(:product, default_price: 100, inventory_level: :very_high, inventory_rates:) }
+
+      it { expect(product.inventory_factor).to eq(-30) }
+    end
+  end
+
   describe "#set_inventory_level" do
-    context "when inventory is below the low bar" do
-      let!(:product) { create(:product, total_inventory: 100, total_reserved: 96) }
+    let(:inventory_thresholds) { { very_low: 0.95, low: 0.80, medium: 0.60, high: 0.40, very_high: 0.20 } }
+
+    context "when inventory is in very low level" do
+      let!(:product) { build(:product, inventory_thresholds:, total_inventory: 100, total_reserved: 95) }
+
+      it { expect(product.set_inventory_level).to eq(:very_low) }
+    end
+
+    context "when inventory is in low level" do
+      let!(:product) { build(:product, inventory_thresholds:, total_inventory: 100, total_reserved: 80) }
 
       it { expect(product.set_inventory_level).to eq(:low) }
     end
 
-    context "when inventory is between the low and high bar" do
-      let!(:product) { create(:product, total_inventory: 100, total_reserved: 50) }
+    context "when inventory is in medium level" do
+      let!(:product) { build(:product, inventory_thresholds:, total_inventory: 100, total_reserved: 60) }
 
       it { expect(product.set_inventory_level).to eq(:medium) }
     end
 
-    context "when inventory is above the high bar" do
-      let!(:product) { create(:product, total_inventory: 100, total_reserved: 10) }
+    context "when inventory is in high level" do
+      let!(:product) { build(:product, inventory_thresholds:, total_inventory: 100, total_reserved: 40) }
 
       it { expect(product.set_inventory_level).to eq(:high) }
+    end
+
+    context "when inventory is in very high level" do
+      let!(:product) { build(:product, inventory_thresholds:, total_inventory: 100, total_reserved: 10) }
+
+      it { expect(product.set_inventory_level).to eq(:very_high) }
+    end
+  end
+
+  describe "#demand_factor" do
+    let(:demand_rates) { { high: 0.10, medium: 0.05, low: 0 } }
+
+    context "when the product demand level is high" do
+      let!(:product) { build(:product, default_price: 100, demand_level: :high, demand_rates:) }
+
+      it { expect(product.demand_factor).to eq(10) }
+    end
+
+    context "when the current demand is equql to the previous demand" do
+      let!(:product) { build(:product, default_price: 100, demand_level: :medium, demand_rates:) }
+
+      it { expect(product.demand_factor).to eq(5) }
+    end
+
+    context "when the current demand is below the previous demand" do
+      let!(:product) { build(:product, default_price: 100, demand_level: :low, demand_rates:) }
+
+      it { expect(product.demand_factor).to eq(0) }
     end
   end
 
   describe "#set_demand_level" do
     context "when the current demand exceeds the previous demand" do
-      let!(:product) { create(:product, current_demand_count: 10, previous_demand_count: 6) }
+      let!(:product) { build(:product, current_demand_count: 10, previous_demand_count: 6) }
 
       it { expect(product.set_demand_level).to eq(:high) }
     end
 
-    context "when the current demand is below the previous demand" do
-      let!(:product) { create(:product, current_demand_count: 1, previous_demand_count: 6) }
+    context "when the current demand is equql to the previous demand" do
+      let!(:product) { build(:product, current_demand_count: 6, previous_demand_count: 6) }
 
-      it { expect(product.set_demand_level).to eq(:low) }
+      it { expect(product.set_demand_level).to eq(:medium) }
     end
 
-    context "when the previous demand is 0" do
-      let!(:product) { create(:product, current_demand_count: 1) }
+    context "when the current demand is below the previous demand" do
+      let!(:product) { build(:product, current_demand_count: 1, previous_demand_count: 6) }
 
       it { expect(product.set_demand_level).to eq(:low) }
     end
